289825c2bbb2509cf2f880d70ec33b913b3e7ce0
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-10-12 16:54:30 +0300

    Adds search function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import './App.css';
+import React, { Component } from 'react';
+import Searchbar from './components/Search/Search'
+import PropTypes from 'prop-types';
+import pixabyApi from './services/pixaby.api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    gallery: [],
+    page: 1,
+    largeImage: '',
+    showModal: false,
+    q: '',
+    isLoading: false,
+    error: null,
+  };
+
+  componentDidUpdate(prevState) {
+    if (prevState.q !== this.state.q) {
+      this.fetchGallery();
+    }
+  }
+
+  onChangeQuery = query => {
+    this.state({ q: query, page: 1, gallery: [], error: null });
+  };
+
+  render(){
+    return(
+
+    {error && <h1>Try again!</h1> }
+    <Searchbar onSubmit={this.onChangeQuery}/>
+
+    )
+  };
 }
 
 export default App;
