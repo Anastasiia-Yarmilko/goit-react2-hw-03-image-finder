b0cc45ad44eeeebb2e0b11c504da461c237a92ad
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2021-10-14 00:30:49 +0300

    fixes

--- src/services/pixaby.api.js
+++ src/services/pixaby.api.js
@@ -1,18 +1,18 @@
-import axios from "axios";
+import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '20808921-1f06a7735b40cece4e6350a25';
 
 axios.defaults.baseURL = BASE_URL;
-axios.defaults.defaults = {
+axios.defaults.params = {
   key: API_KEY,
   image_type: 'proto',
   orientation: 'horizontal',
-  per_page: 12
-}
+  per_page: 12,
+};
 
-const fetchPixabyImages = async({q, page}) => {
-  return await axios.get('', {params: {q, page}})
+const fetchPixabyImages = async ({ q, page }) => {
+  return await axios.get('', { params: { q, page } });
 };
 
 export default fetchPixabyImages;
